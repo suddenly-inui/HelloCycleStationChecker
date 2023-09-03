06a7611a6bfa8a60caab948ec482e7d827a0c288
suddenly_inui <inuiyuki0904@gmail.com>
2023-09-03 23:09:05 +0900

    feat: implemented search function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,37 @@
 <template>
   <h1>ステーション名を検索</h1>
   <input type="text" v-model="station_name" />
-  <StationInfo :station_name="station_name"></StationInfo>
+  <button @click="search">検索</button>
+  <StationInfo :station_name="name" v-for="name in station_name_list" :key="name">{{ name }}</StationInfo>
 </template>
 
 <script setup lang="ts">
 import StationsApiService from '@/services/StationsApiService'
 import StationInfo from '@/components/StationInfo.vue'
-import { ref, watch } from 'vue'
-import type { StationResponse, Station } from '../types/response'
+import { onMounted, ref } from 'vue'
+import type { Stations } from '../types/response'
 
-
-let stations = ref<StationResponse>()
+let stations = ref()
 let station_name = ref('')
+let station_name_list = ref<string[]>([])
 
-StationsApiService.getAll().then((data) => {
-  stations.value = data
+onMounted(() => {  
+  StationsApiService.getAll().then((data) => {
+    stations.value = data.data!.stations
+  })
 })
 
-
-let station = ref<Station|null>(null)
-watch(station_name, async (val) => {
-  if (val) {
-    const response = await StationsApiService.get(val)
-    station.value = response.data.station[0] as Station | null
+const search = async () => {  
+  if (station_name.value.trim() === "") {
+    return
   }
-})
+  station_name_list.value = []
+  for (let key in stations.value) {    
+    if (key.includes(station_name.value.trim())) {
+      station_name_list.value.push(key)
+    }
+  }
+}
 </script>
 
 <style></style>
