e4545ed136f692958600499e43805f5e84c9e9ae
suddenly_inui <inuiyuki0904@gmail.com>
2023-09-04 23:41:25 +0900

    feat: made station container component

--- src/views/FavouriteStation.vue
+++ src/views/FavouriteStation.vue
@@ -1,9 +1,12 @@
 <template>
 	<h1 class="page_name">FAVOURITES</h1>
-	<StationInfo v-for="fav in fav_station" :key="fav" :station_name="fav"></StationInfo>
+	<StationCardContainer>
+		<StationInfo v-for="fav in fav_station" :key="fav" :station_name="fav"></StationInfo>
+	</StationCardContainer>
 </template>
 
 <script setup lang="ts">
+import StationCardContainer from '@/components/StationCardContainer.vue';
 import StationInfo from '@/components/StationInfo.vue';
 import { fav_station } from '@/services/LocalStorage';
 </script>
