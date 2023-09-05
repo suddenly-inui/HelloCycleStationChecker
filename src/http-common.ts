7c0d0778ba096d04db4f51ec22cff1b5665bbbe1
suddenly_inui <inuiyuki0904@gmail.com>
2023-09-05 17:37:45 +0900

    fix: update base url

--- src/http-common.ts
+++ src/http-common.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   // APIのURI
-  baseURL: 'https://60cd-240d-1a-55c-1403-80a0-d628-1d9d-52a5.ngrok-free.app',
+  baseURL: 'https://daed-133-27-186-11.ngrok-free.app',
   // リクエストヘッダ
   headers: {
     'Content-type': 'application/json'
