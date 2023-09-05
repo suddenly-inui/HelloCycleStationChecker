fae5e7bc925c2de48d84a3789e9c378d26ef8960
suddenly_inui <inuiyuki0904@gmail.com>
2023-09-05 14:07:22 +0900

    fix: changed baseURL to ngrok

--- src/http-common.ts
+++ src/http-common.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   // APIのURI
-  baseURL: 'http://34.85.49.75/',
+  baseURL: 'https://60cd-240d-1a-55c-1403-80a0-d628-1d9d-52a5.ngrok-free.app',
   // リクエストヘッダ
   headers: {
     'Content-type': 'application/json'
